8d0da9915e7d0d1094e473e86474ce051d41479b
David-Jr <david0gjunior@gmail.com>
2023-06-06 22:32:02 +0200

    Creation of TabBar.

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -1,17 +1,54 @@
+import 'package:ativa_ja/pages/blind/blind_friendly_mode.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import '../widgets/ativa_ja_widget.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<RootPage> createState() => _RootPageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _RootPageState extends State<RootPage> {
+  int activePointers = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Listener(
+      onPointerDown: (event) {
+        setState(() {
+          activePointers++;
+        });
+      },
+      onPointerUp: (event) {
+        setState(() {
+          activePointers--;
+        });
+      },
+      onPointerMove: (event) {
+        if (activePointers == 2 && event.delta.dy > 0) {
+          activePointers = 0;
+          Get.to(() => const BlindeFriendlyMode());
+        }
+      },
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: const AtivaJaWidget(),
+            bottom: const TabBar(
+              indicatorSize: TabBarIndicatorSize.label,
+              tabs: [
+                Tab(icon: Icon(Icons.home), iconMargin: EdgeInsets.zero),
+                Tab(icon: Icon(Icons.favorite), iconMargin: EdgeInsets.zero),
+                Tab(icon: Icon(Icons.history), iconMargin: EdgeInsets.zero),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
