a4c38cb72b80f0bee98619acb742bd9e2cbf3117
David-Jr <david0gjunior@gmail.com>
2023-06-06 23:40:36 +0200

    Changed isp from int to Isp, favorite from bool to Rxool.

--- lib/models/shortcut.dart
+++ lib/models/shortcut.dart
@@ -1,13 +1,17 @@
 import 'dart:convert';
 
+import 'package:get/get.dart';
+
+import 'isp.dart';
+
 class Shortcut {
   int id;
   String tittle;
   String steps;
   String ussdCode;
   String description;
-  int ispCode;
-  bool isFavorite;
+  Isp isp;
+  RxBool isFavorite;
 
   Shortcut({
     required this.id,
@@ -15,46 +19,43 @@ class Shortcut {
     required this.steps,
     required this.ussdCode,
     required this.description,
-    required this.ispCode,
+    required this.isp,
     required this.isFavorite,
   });
 
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
+  
 
-    return other is Shortcut &&
-        other.id == id &&
-        other.tittle == tittle &&
-        other.steps == steps &&
-        other.ussdCode == ussdCode &&
-        other.description == description &&
-        other.ispCode == ispCode &&
-        other.isFavorite == isFavorite;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^
-        tittle.hashCode ^
-        steps.hashCode ^
-        ussdCode.hashCode ^
-        description.hashCode ^
-        ispCode.hashCode ^
-        isFavorite.hashCode;
+  Shortcut copyWith({
+    int? id,
+    String? tittle,
+    String? steps,
+    String? ussdCode,
+    String? description,
+    Isp? isp,
+    RxBool? isFavorite,
+  }) {
+    return Shortcut(
+      id: id ?? this.id,
+      tittle: tittle ?? this.tittle,
+      steps: steps ?? this.steps,
+      ussdCode: ussdCode ?? this.ussdCode,
+      description: description ?? this.description,
+      isp: isp ?? this.isp,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
   }
 
   Map<String, dynamic> toMap() {
     final result = <String, dynamic>{};
-
+  
     result.addAll({'id': id});
     result.addAll({'tittle': tittle});
     result.addAll({'steps': steps});
     result.addAll({'ussdCode': ussdCode});
     result.addAll({'description': description});
-    result.addAll({'ispCode': ispCode});
+    result.addAll({'isp': isp.toMap()});
     result.addAll({'isFavorite': isFavorite});
-
+  
     return result;
   }
 
@@ -65,38 +66,42 @@ class Shortcut {
       steps: map['steps'] ?? '',
       ussdCode: map['ussdCode'] ?? '',
       description: map['description'] ?? '',
-      ispCode: map['ispCode']?.toInt() ?? 0,
-      isFavorite: map['isFavorite'] ?? false,
+      isp: Isp.fromMap(map['isp']),
+      isFavorite: map['isFavorite'],
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory Shortcut.fromJson(String source) =>
-      Shortcut.fromMap(json.decode(source));
-
-  Shortcut copyWith({
-    int? id,
-    String? tittle,
-    String? steps,
-    String? ussdCode,
-    String? description,
-    int? ispCode,
-    bool? isFavorite,
-  }) {
-    return Shortcut(
-      id: id ?? this.id,
-      tittle: tittle ?? this.tittle,
-      steps: steps ?? this.steps,
-      ussdCode: ussdCode ?? this.ussdCode,
-      description: description ?? this.description,
-      ispCode: ispCode ?? this.ispCode,
-      isFavorite: isFavorite ?? this.isFavorite,
-    );
-  }
+  factory Shortcut.fromJson(String source) => Shortcut.fromMap(json.decode(source));
 
   @override
   String toString() {
-    return 'Shortcut(id: $id, tittle: $tittle, steps: $steps, ussdCode: $ussdCode, description: $description, ispCode: $ispCode, isFavorite: $isFavorite)';
+    return 'Shortcut(id: $id, tittle: $tittle, steps: $steps, ussdCode: $ussdCode, description: $description, isp: $isp, isFavorite: $isFavorite)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Shortcut &&
+      other.id == id &&
+      other.tittle == tittle &&
+      other.steps == steps &&
+      other.ussdCode == ussdCode &&
+      other.description == description &&
+      other.isp == isp &&
+      other.isFavorite == isFavorite;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      tittle.hashCode ^
+      steps.hashCode ^
+      ussdCode.hashCode ^
+      description.hashCode ^
+      isp.hashCode ^
+      isFavorite.hashCode;
   }
 }
