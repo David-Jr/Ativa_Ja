056f7b44bcb7aee6f03fe0b4ed4457c1a3d53429
David-Jr <david0gjunior@gmail.com>
2023-06-08 12:07:21 +0200

    Placeholder removal.

--- lib/pages/history/history_page.dart
+++ lib/pages/history/history_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../controllers/data_controller.dart';
+import '../../widgets/shortcut_widget.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
@@ -8,10 +12,19 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  final shortcuts = Get.find<ListsController>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      //todo
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Obx(
+        () => ListView.builder(
+          itemCount: shortcuts.shortcutList.length,
+          itemBuilder: (context, index) {
+            return ShortcutWidget(shortcut: shortcuts.shortcutList[index]);
+          },
+        ),
+      ),
     );
   }
 }
