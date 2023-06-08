34654b7b005594b89e751bd4b30a92659e3ba3d6
David-Jr <david0gjunior@gmail.com>
2023-06-08 08:35:25 +0200

    Removed the placeholder

--- lib/pages/favorites/favorites_page.dart
+++ lib/pages/favorites/favorites_page.dart
@@ -1,4 +1,8 @@
+import 'package:ativa_ja/controllers/data_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../widgets/shortcut_widget.dart';
 
 class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
@@ -8,10 +12,16 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
+  final shortcuts = Get.find<ListsController>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-        //todo
-        );
+    return Obx(
+        () => ListView.builder(
+          itemCount: shortcuts.favoriteShortcuts.length,
+          itemBuilder: (context, index) {
+            return ShortcutWidget(shortcut: shortcuts.favoriteShortcuts[index]);
+          },
+        ),
+      );
   }
 }
